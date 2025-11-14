7309ee837b72a4bacc0b68795d5b529765319d5a
Joao Silva <joaosilvaa.work@gmail.com>
2025-11-14 18:27:21 +0000

    count_chars new file

--- count_chars.py
+++ count_chars.py
@@ -1,9 +1,17 @@
+#guarda o input que o utilizador dá
 frase = input()
 
+#cria um dicionário vazio
 dicionario = {}
+
+#ciclo for para percorrer cada carácter da frase,,um de cada vez
+#verifica se a letra ainda não foi adicionada ao dicionário
+#conta quantas vezes a letra aparece e guarda no dicionário
 for letra in frase:
     if letra not in dicionario:
         dicionario[letra] = frase.count(letra)
 
+
+#mostra o dicionário obtido
 print(dicionario)
 
